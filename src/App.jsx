0b68a7728f9cf89aa42808400ad37b6eca37bab1
siran-1 <siranjeevik21@gmail.com>
2025-01-18 18:30:15 +0000

    adding google sheet for likes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import PhotoGallery from "./components/PhotoGallery"
@@ -5,23 +7,29 @@ import About from "./components/About"
 import Work from "./components/Work"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import Blog from './components/Blog';
 
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <main>
-        <Hero/>
-        <PhotoGallery/>
-        <About/>
-        <Work/>
-        <Contact/>
-      </main>
-      <Footer/>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={
+                    <main>
+                        <Hero />
+                        <PhotoGallery />
+                        <About />
+                        <Work />
+                        <Contact />
+                        <Footer />
+                    </main>
+                } />
+                <Route path="/blog/:id" element={<Blog />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
